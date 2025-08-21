2a5a72ffacc6a80917aebf4ce5e4b541cc3f76a8
VillePajala <valoraami@gmail.com>
2025-08-21 10:16:02 +0300

    refactor: remove unused Image import from StartScreen component
    - Deleted the unused Image import statement in StartScreen.tsx to clean up the code.
    - This change helps streamline the component and improve readability.

--- src/components/StartScreen.tsx
+++ src/components/StartScreen.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
 import { useTranslation } from 'react-i18next';
 import i18n from '@/i18n';
 import {
