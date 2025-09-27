fc1c4f3c984fda394dbaf66067ee77eb32b3cd69
VillePajala <valoraami@gmail.com>
2025-09-27 21:57:35 +0300

    build: update release notes and service worker timestamp

--- public/sw.js
+++ public/sw.js
@@ -73,4 +73,4 @@ self.addEventListener('fetch', (event) => {
     event.respondWith(fetch(request));
   }
 });
-// Build Timestamp: 2025-09-27T07:22:44.234Z
+// Build Timestamp: 2025-09-27T17:55:42.648Z
